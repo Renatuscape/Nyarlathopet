6012e0729f7e8a3ee0afc064e114da849ecddaa0
Renatuscape <silarcta@gmail.com>
2025-01-06 09:16:57 +0100

    Adds a class to randomly generate human names

---
+++ Assets/Scripts/GameManager/CharacterFactory/HumanNameGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+
+// Sorts and holds human name data, and gives out a random name when called
+public static class HumanNameGenerator
+{
+    public static string[] FirstNames { get; private set; }
+    public static string[] LastNames { get; private set; }
+
+    public static void SortNames(HumanNamesData namesData)
+    {
+        FirstNames = namesData.firstNames.Split(", ").ToArray();
+        LastNames = namesData.lastNames.Split(", ").ToArray();
+    }
+
+    public static string GetRandomName()
+    {
+        return FirstNames[Random.Range(0, FirstNames.Length)] + " " + LastNames[Random.Range(0, FirstNames.Length)];
+    }
+}
