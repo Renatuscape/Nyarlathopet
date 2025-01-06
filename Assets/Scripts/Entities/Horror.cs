cf8481fd5d29016f08a08450965875edea278ecf
Renatuscape <silarcta@gmail.com>
2025-01-06 12:27:11 +0100

    Add id variable to horror

--- Assets/Scripts/Entities/Horror.cs
+++ Assets/Scripts/Entities/Horror.cs
@@ -1,7 +1,9 @@
+// Horrors are versions of Nyarlathotep, loaded in from JSON
 [System.Serializable]
 public class Horror : Creature
 {
-    public int rage;
+    public int id; // Generated on load
+    public int rage; // Changes depending on player action
     public int intrigue;
     public int abstraction;
     public int magick;
