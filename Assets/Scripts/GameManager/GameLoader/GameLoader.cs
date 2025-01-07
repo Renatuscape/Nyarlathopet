bde3ef0c10b2aa4bd3be955eaad5b07c3cb1d3c6
Renatuscape <silarcta@gmail.com>
2025-01-07 12:29:35 +0100

    Add async loading logic and loading message and bar activity

--- Assets/Scripts/GameManager/GameLoader/GameLoader.cs
+++ Assets/Scripts/GameManager/GameLoader/GameLoader.cs
@@ -1,18 +1,48 @@
+
 using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider loadBar;
+    public HumanNameLoader humanNameLoader;
+    public SaveDataLoader saveDataLoader;
+    public TextMeshProUGUI loaderDescription;
+
     void Start()
     {
-        
+        StartCoroutine(LoadRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadRoutine()
     {
-        
+        // Set up loadbar
+        loadBar.value = 0;
+        loadBar.maxValue = 3; // Should equal amount of loaders. Find way to automate this
+
+        // Load components
+        loaderDescription.text = "Transcribing human names...";
+        yield return StartCoroutine(humanNameLoader.LoadData());
+        Report.Write(name, "Loaded human names from JSON.");
+        loadBar.value++;
+
+        yield return new WaitForSeconds(1); // emulate loading. Replace with real loader
+        loadBar.value++;
+
+        loaderDescription.text = "Fetching cult records...";
+        yield return StartCoroutine(saveDataLoader.LoadData());
+        Report.Write(name, "Loaded save data from JSON.");
+        loadBar.value++;
+
+        yield return new WaitForSeconds(1); // emulate loading. Replace with real loader
+        loadBar.value++;
+        DebugManager.WriteDebugSessionLog(true);
+
+
+        Report.Write(name, "Completed load routine.");
+        SceneManager.LoadScene("MainMenu");
     }
 }
