660f88ec5bfe2b40045ba4b1676ea472aa84c301
Renatuscape <silarcta@gmail.com>
2025-01-07 12:28:51 +0100

    Add save data loader to retrieve player data

---
+++ Assets/Scripts/GameManager/GameLoader/JsonLoaders/SaveDataLoader.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveDataWrapper
+{
+    public PlayerData playerData;
+}
+
+// Loads the player's save data from JSON
+// Currently there is only one save data file, but more could be added in the wrapper
+public class SaveDataLoader : MonoBehaviour
+{
+    [SerializeField] private TextAsset saveDataJSON;
+
+    public IEnumerator LoadData()
+    {
+        if (saveDataJSON == null)
+        {
+            Report.Write(name, "SaveData.json is missing or not assigned.");
+            yield break;
+        }
+
+        SaveDataWrapper saveData = JsonUtility.FromJson<SaveDataWrapper>(saveDataJSON.text);
+        yield return null;
+    }
+}
+
