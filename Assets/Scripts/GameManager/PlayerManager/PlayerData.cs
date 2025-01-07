a2640e5c81834740c6d19e4ea1c1706139a5166b
Renatuscape <silarcta@gmail.com>
2025-01-07 13:35:36 +0100

    Add ToString() override for player data to easily print to console

--- Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
+++ Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
@@ -12,4 +12,9 @@ public class PlayerData
     public List<Item> inventory = new(); // Holds inventory items, which are randomly generated
     public List<int> discoveredCreatures = new(); // Holds ID of discovered creatures
     public Horror currentCreature;
+
+    public override string ToString()
+    {
+        return $"{cultName ?? "Unnamed Cult"} ({level})\nLeader\t\t{cultLeader.name}\nFunds:\t\t{funds}\nMembers:\t{cultMembers.Count}";
+    }
 }
