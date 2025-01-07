e6aab2d7a11585b30f8784343da2296f1607fe74
Renatuscape <silarcta@gmail.com>
2025-01-07 13:34:58 +0100

    Make CreateNewGameData async and add call to save data after creation

--- Assets/Scripts/GameManager/PlayerManager/Player.cs
+++ Assets/Scripts/GameManager/PlayerManager/Player.cs
@@ -1,14 +1,22 @@
+using System.Threading.Tasks;
+
 public static class Player
 {
     public static PlayerData Data { get; private set; }
 
-    public static void CreateNewGameData()
+    public static async Task CreateNewGameDataAsync()
     {
-        Data = new();
+        Data = new()
+        {
+            cultName = "Curators of the Masks",
+            level = 1,
+            funds = 100,
+            cultLeader = CultistFactory.GetCultist(1),
+            cultMembers = new(),
+            inventory = new(),
+        };
 
-        Data.cultName = "Curators of the Masks";
-        Data.level = 1;
-        Data.funds = 100;
-        Data.cultLeader = CultistFactory.GetCultist(1);
+        Report.Write("Player", "Created save data: " + Data.ToString());
+        await SaveManager.SaveDataAsync();
     }
 }
