3afc121c4ee97eea35ec385d66911d73f00c6c97
Renatuscape <silarcta@gmail.com>
2025-01-07 15:47:13 +0100

    Add skill generation to new cultists

--- Assets/Scripts/GameManager/CharacterFactory/CultistFactory.cs
+++ Assets/Scripts/GameManager/CharacterFactory/CultistFactory.cs
@@ -9,8 +9,30 @@ public static class CultistFactory
         {
             name = HumanNameGenerator.GetRandomName(),
             type = CreatureType.Cultist,
-            funds = random.Next(0, (level * 5) + 1)
+            funds = random.Next(0, (level * 5) + 1),
+            sanity = random.Next(5, (level * 10) + 1)
         };
+
+        int skillPoints = level * 2;
+
+        while (skillPoints > 0)
+        {
+            int randomStat = random.Next(0, 3);
+            if (randomStat == 0)
+            {
+                cultist.magick++;
+            }
+            else if (randomStat == 1)
+            {
+                cultist.strength++;
+            }
+            else
+            {
+                cultist.lore++;
+            }
+
+            skillPoints--;
+        }
 
         return cultist;
     }
