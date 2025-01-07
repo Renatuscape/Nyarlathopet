0aba31717ea352862e62663293ff1e9421bb2b12
Renatuscape <silarcta@gmail.com>
2025-01-07 12:30:21 +0100

    Initialise lists when new PlayerData is created

--- Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
+++ Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
@@ -8,8 +8,8 @@ public class PlayerData
     public int level;
     public int funds; // The cult's pooled funds
     public Human cultLeader; // The current player character
-    public List<Human> cultMembers;
-    public List<Item> inventory; // Holds inventory items, which are randomly generated
-    public List<int> discoveredCreatures; // Holds ID of discovered creatures
+    public List<Human> cultMembers = new();
+    public List<Item> inventory = new(); // Holds inventory items, which are randomly generated
+    public List<int> discoveredCreatures = new(); // Holds ID of discovered creatures
     public Horror currentCreature;
 }
