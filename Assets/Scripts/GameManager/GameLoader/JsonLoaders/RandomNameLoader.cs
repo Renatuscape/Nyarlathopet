1e17c018d73e925e195b11c4230cfc2d58d0cbb7
Renatuscape <silarcta@gmail.com>
2025-01-08 16:10:33 +0100

    Rename HumanNameLoader to RandomNameLoader to reflect data changes

---
+++ Assets/Scripts/GameManager/GameLoader/JsonLoaders/RandomNameLoader.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class RandomNamesData
+{
+    public string firstNames;
+    public string lastNames;
+    public string cultPrefixes;
+    public string cultDefiniteSuffixes;
+    public string cultIndefiniteSuffixes;
+    public string cultNames;
+}
+
+// Loads in first and last names for humans at the start of the game, and holds them for future use
+public class RandomNameLoader : MonoBehaviour
+{
+    [SerializeField] private TextAsset RandomNamesJSON;
+
+    public IEnumerator LoadData()
+    {
+        Report.Write(name, "Loading random name data.");
+
+        if (RandomNamesJSON == null)
+        {
+            Report.Write(name, "RandomNames.json is missing or not assigned.");
+            yield break;
+        }
+
+        RandomNamesData namesData = JsonUtility.FromJson<RandomNamesData>(RandomNamesJSON.text);
+        RandomNameGenerator.SortNames(namesData);
+        yield return null;
+    }
+}
