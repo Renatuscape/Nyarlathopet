cceae91e9cc8505d3313856a5817df8d163ec0d5
Renatuscape <silarcta@gmail.com>
2025-01-09 11:09:53 +0100

    Add sanityLoss variable and documentation

--- Assets/Scripts/Entities/Horror.cs
+++ Assets/Scripts/Entities/Horror.cs
@@ -1,11 +1,14 @@
 // Horrors are versions of Nyarlathotep, loaded in from JSON
+// For pets, the stats indicate needed materials in order to summon them
+// For Nyarlathotep, the stats indicates minimum stat growth needed
 [System.Serializable]
 public class Horror : Creature
 {
     public int id; // Generated on load
-    public int rage; // Changes depending on player action
+    public int rage; // Base for pets, added upon summoning for Nyarlathotep
     public int intrigue;
     public int abstraction;
     public int magick;
     public int strength;
+    public int sanityLoss; // Max potential sanity loss upon witnessing the horror
 }
