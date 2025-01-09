42a1776eba130ac606984d71e2974f675eb369f8
Renatuscape <silarcta@gmail.com>
2025-01-09 16:05:25 +0100

    Add network and notoriety variable, rename currentCreature to currentPet for consistency

--- Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
+++ Assets/Scripts/GameManager/PlayerManager/PlayerData.cs
@@ -7,12 +7,14 @@ public class PlayerData
     public string cultName;
     public int level;
     public int rounds;
-    public int funds; // The cult's pooled funds
-    public Human cultLeader; // The current player character
+    public int funds;           // The cult's pooled funds
+    public int network;         // Can be used instead of funds or members, but greatly increases notoriety
+    public int notoriety;       // The cult's public notoriety. Too high and your plots will be thwarted!
+    public Human cultLeader;    // The current player character
     public List<Human> cultMembers = new();
     public List<Item> inventory = new(); // Holds inventory items, which are randomly generated
     public List<int> discoveredCreatures = new(); // Holds ID of discovered creatures
-    public Horror currentCreature;
+    public Horror currentPet;
 
     public override string ToString()
     {
