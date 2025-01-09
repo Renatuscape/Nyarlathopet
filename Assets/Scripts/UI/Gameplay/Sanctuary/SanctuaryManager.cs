76b065e09a2c751061406349bc60ba16fe957b9f
Renatuscape <silarcta@gmail.com>
2025-01-09 16:07:12 +0100

    Add class for managing Sanctuary menu

---
+++ Assets/Scripts/UI/Gameplay/Sanctuary/SanctuaryManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanctuaryManager : MonoBehaviour
+{
+    public TextMeshProUGUI petName;
+    public TextMeshProUGUI petStats;
+
+    public Button btnFeed;
+    public Button btnWorship;
+    public Button btnCommune;
+    public Button btnBeginRitual;
+
+    private void OnEnable()
+    {
+        if (Player.Data?.currentPet != null)
+        {
+            SetupWithPet(Player.Data.currentPet);
+        }
+        else
+        {
+            SetupWithoutPet();
+        }
+    }
+
+    void SetupWithPet(Horror pet)
+    {
+        btnFeed.gameObject.SetActive(true);
+        btnWorship.gameObject.SetActive(true);
+        btnCommune.gameObject.SetActive(true);
+        btnBeginRitual.gameObject.SetActive(false);
+
+        petName.text = pet.name;
+        petName.text = $"{pet.rage:D2} Rage\n\n\n" +
+                        $"{pet.intrigue:D2} Intrigue\n\n" +
+                        $"{pet.abstraction:D2} Abstraction\n\n" +
+                        $"{pet.strength:D2} Strength\n\n" +
+                        $"{pet.magick:D2} Magick\n\n";
+        petName.gameObject.SetActive(true);
+    }
+
+    void SetupWithoutPet()
+    {
+        petName.gameObject.SetActive(false);
+        
+        petStats.text = "The sanctuary is ready for a summoning.";
+
+        btnFeed.gameObject.SetActive(false);
+        btnWorship.gameObject.SetActive(false);
+        btnCommune.gameObject.SetActive(false);
+        btnBeginRitual.gameObject.SetActive(true);
+    }
+}
