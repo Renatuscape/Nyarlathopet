9cb9dee01f64d6610f3cf41662fbe1da00cd87df
Renatuscape <silarcta@gmail.com>
2025-01-09 16:05:59 +0100

    Remove spell type for simplicity

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,14 +1,18 @@
 [System.Serializable]
 public class Item
 {
+    public int level;
     public string name;
     public ItemType type;
+
+    public int magick;
+    public int strength;
+    public int lore;
 }
 
 public enum ItemType
 {
     Artefact,
     Tome,
-    Spell,
     Prayer
 }
