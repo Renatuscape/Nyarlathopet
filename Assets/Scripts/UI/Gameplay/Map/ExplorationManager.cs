56e532defb011f67e6459a333793d87d87b0967b
Renatuscape <silarcta@gmail.com>
2025-01-10 13:25:27 +0100

    Replace ExplorationController with ExplorationManager class

---
+++ Assets/Scripts/UI/Gameplay/Map/ExplorationManager.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Progress;
+public static class ExplorationManager
+{
+    public static void SeekArtefacts(Location location)
+    {
+        // Add costs here
+
+        Item item = ItemFactory.GenerateItemFromLocation(location);
+        GameplayManager.dummyData.inventory.Add(item);
+
+        AlertSystem.Force($"You obtained {item}", () =>
+        {
+            HandleEndeavourPoints();
+        });
+    }
+    public static void ThwartEnemies()
+    {
+        // Reduce notoriety against a random cost of cultists and network
+
+        HandleEndeavourPoints();
+    }
+
+    internal static void RecruitMembers(Location currentLocation)
+    {
+        // Add costs here
+        // Add chance of increased network
+
+        Human cultist = CultistFactory.GetCultist(currentLocation.level);
+        cultist.origin = currentLocation.name;
+        GameplayManager.dummyData.cultMembers.Add(cultist);
+
+        AlertSystem.Force($"{cultist.name} joined your cult.", () =>
+        {
+            HandleEndeavourPoints();
+        });
+    }
+
+    static void HandleEndeavourPoints()
+    {
+        // In the future, it should be possible to spend multiple endeavour points
+        GameplayManager.endeavourPoints--;
+
+        if (GameplayManager.endeavourPoints <= 0)
+        {
+            GameplayManager.EndRound();
+        }
+    }
+}
