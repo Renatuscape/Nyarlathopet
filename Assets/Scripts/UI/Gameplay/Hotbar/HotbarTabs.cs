27993796201276e107fe7ae9ec9af1f28a88920a
Renatuscape <silarcta@gmail.com>
2025-01-10 13:32:04 +0100

    Add hotbar text reference and refresh method

--- Assets/Scripts/UI/Gameplay/Hotbar/HotbarTabs.cs
+++ Assets/Scripts/UI/Gameplay/Hotbar/HotbarTabs.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class HotbarController : MonoBehaviour
 {
+    public HotbarText hotbarText;
     public Button btnSanctuary;
     public Button btnLedger;
     public Button btnMap;
@@ -17,16 +18,32 @@ public class HotbarController : MonoBehaviour
 
     void Start()
     {
-        Initialise();
+        StartCoroutine(Initialise());
     }
 
-    void Initialise()
+    IEnumerator Initialise()
     {
+        while (GameplayManager.instance == null)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+
         ToggleView(currentView);
 
         btnSanctuary.onClick.AddListener(() => ToggleView(0));
         btnLedger.onClick.AddListener(() => ToggleView(1));
         btnMap.onClick.AddListener(() => ToggleView(2));
+
+        hotbarText.UpdateDisplay();
+    }
+
+    public void RefreshForNewRound()
+    {
+        ToggleView(-1);
+
+        hotbarText.UpdateDisplay();
+
+        ToggleView(0);
     }
 
     void ToggleView(int view)
