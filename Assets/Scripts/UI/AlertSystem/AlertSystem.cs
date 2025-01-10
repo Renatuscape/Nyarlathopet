9f7b8937a3d44ef96cf4fed873b33d6987d7015d
Renatuscape <silarcta@gmail.com>
2025-01-10 13:30:31 +0100

    Add method to open alert with action only, and no close button. Toggle button visibility appropriately.

--- Assets/Scripts/UI/AlertSystem/AlertSystem.cs
+++ Assets/Scripts/UI/AlertSystem/AlertSystem.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 
-// Dynamic class for giving the player text alerts and confirmation prompts. Ensure that an AlertSystem object exists in the scene so static methods can be called
+// Used by other classes to display text alerts and confirmation prompts. Ensure that an AlertSystem object exists in the scene so static methods can be called
 public class AlertSystem : MonoBehaviour
 {
     static AlertSystem instance;
@@ -24,12 +22,35 @@ public class AlertSystem : MonoBehaviour
 
     void PrintAlert(string alertText)
     {
+        btnCloseAlert.gameObject.SetActive(true);
+        btnConfirmAction.gameObject.SetActive(false);
+
         alertMesh.text = alertText;
         alertCanvas.gameObject.SetActive(true);
     }
 
     void PrintActionPrompt(string alertText, Action action)
     {
+        btnCloseAlert.gameObject.SetActive(true);
+        btnConfirmAction.gameObject.SetActive(true);
+
+        alertMesh.text = alertText;
+        alertCanvas.gameObject.SetActive(true);
+
+        btnConfirmAction.onClick.RemoveAllListeners();
+        btnConfirmAction.onClick.AddListener(() =>
+        {
+            Report.Write(name, "Attempting to invoke action assigned to Confirm button: " + action.Method?.Name ?? "unnamed");
+            CloseAlert();
+            action.Invoke();
+        });
+    }
+
+    void PrintForceAction(string alertText, Action action)
+    {
+        btnCloseAlert.gameObject.SetActive(false);
+        btnConfirmAction.gameObject.SetActive(true);
+
         alertMesh.text = alertText;
         alertCanvas.gameObject.SetActive(true);
 
@@ -65,5 +86,15 @@ public class AlertSystem : MonoBehaviour
             return;
         }
         instance.PrintActionPrompt(alertText, action);
+    }
+
+    public static void Force(string alertText, Action action)
+    {
+        if (instance == null)
+        {
+            Report.Write("AlertSystem static", "Attempted to call AlertSystem while instance was null.");
+            return;
+        }
+        instance.PrintForceAction(alertText, action);
     }
 }
