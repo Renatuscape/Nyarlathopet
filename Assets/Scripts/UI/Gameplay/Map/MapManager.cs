5da3d95d0cca9097ad766c215baa38a1d9ea9ffe
Renatuscape <silarcta@gmail.com>
2025-01-10 13:32:24 +0100

    Add functionality to map view

--- Assets/Scripts/UI/Gameplay/Map/MapManager.cs
+++ Assets/Scripts/UI/Gameplay/Map/MapManager.cs
@@ -1,18 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MapManager instance;
+    public TextMeshProUGUI infoTitle;
+    public TextMeshProUGUI infoMesh;
+    public Location currentLocation = new() { name = "Amsterdam", hasMagick = true};
+
+    public Button btnSeek;
+    public Button btnRecruit;
+    public Button btnThwart;
+
+    private void Start()
     {
-        
+        instance = this;
+        btnSeek.onClick.AddListener(() => BtnSeek());
+        btnThwart.onClick.AddListener(() => BtnThwart());
+        btnRecruit.onClick.AddListener(() => BtnRecruit());
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnEnable()
     {
-        
+        RefreshInfo();
+    }
+
+    void RefreshInfo()
+    {
+        if (currentLocation != null && !string.IsNullOrEmpty(currentLocation.name))
+        {
+            infoTitle.text = (currentLocation.isRisky ? "!" : "") + currentLocation.name + (currentLocation.isRisky ? "!" : "");
+            infoMesh.text = currentLocation.description;
+
+            btnSeek.gameObject.SetActive(true);
+            btnRecruit.gameObject.SetActive(true);
+            btnThwart.gameObject.SetActive(true);
+        }
+        else
+        {
+            infoTitle.text = "Endeavour";
+            infoMesh.text = "Select a destination to proceed with your endeavour. Risky locations yield higher rewards.";
+
+            btnSeek.gameObject.SetActive(false);
+            btnRecruit.gameObject.SetActive(false);
+            btnThwart.gameObject.SetActive(false);
+        }
+    }
+
+    void BtnSeek()
+    {
+        if (currentLocation == null)
+        {
+            AlertSystem.Print("Select your destination on the map.");
+        }
+        else
+        {
+            AlertSystem.Prompt($"Start an endeavour to seek hidden artefacts in {currentLocation.name}?", () =>
+            {
+                ExplorationManager.SeekArtefacts(currentLocation);
+            });
+        }
+    }
+
+    void BtnThwart()
+    {
+        AlertSystem.Prompt($"Expend your cultists and connections to thwart your enemies?", () =>
+        {
+            ExplorationManager.ThwartEnemies();
+        });
+    }
+
+    void BtnRecruit()
+    {
+        if (currentLocation == null)
+        {
+            AlertSystem.Print("Select your destination on the map.");
+        }
+        else
+        {
+            AlertSystem.Prompt($"Start an endeavour to recruit members in {currentLocation.name}?", () =>
+            {
+                ExplorationManager.RecruitMembers(currentLocation);
+            });
+        }
+    }
+
+    public static void SetDestination(Location location)
+    {
+        instance.currentLocation = location;
+        instance.RefreshInfo();
     }
 }
