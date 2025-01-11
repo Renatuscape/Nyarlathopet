035a815067e19401247fede2355330879e5c5690
Renatuscape <rrjonsberg@gmail.com>
2025-01-11 15:35:45 +0100

    Partially implement thwart enemies. Improve SeekArtefacts with notoriety and network gain.

--- Assets/Scripts/UI/Gameplay/Map/ExplorationManager.cs
+++ Assets/Scripts/UI/Gameplay/Map/ExplorationManager.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Progress;
+using Random = UnityEngine.Random;
 public static class ExplorationManager
 {
     public static void SeekArtefacts(Location location)
@@ -12,16 +9,74 @@ public static class ExplorationManager
         Item item = ItemFactory.GenerateItemFromLocation(location);
         GameplayManager.dummyData.inventory.Add(item);
 
-        AlertSystem.Force($"You obtained {item.name}", () =>
+        string alertMessage = $"You obtained {item.name}.";
+        int networkGain = Random.Range(0, item.level + 1);
+        int notorietyGain = Random.Range(0, item.level + 1);
+
+        if (networkGain > 0)
+        {
+            alertMessage += $"\nYour endeavours improved your network by " + networkGain;
+            GameplayManager.dummyData.network += networkGain;
+        }
+
+        if (notorietyGain > 0)
+        {
+            alertMessage += $"\nThose who would seek to stop you have taken notice. Notoriety increased by " + notorietyGain;
+            GameplayManager.dummyData.notoriety += notorietyGain;
+        }
+
+        AlertSystem.Force(alertMessage, () =>
         {
             HandleEndeavourPoints();
         });
     }
+
     public static void ThwartEnemies()
     {
         // Reduce notoriety against a random cost of cultists and network
 
-        HandleEndeavourPoints();
+        if (GameplayManager.dummyData.notoriety < 1)
+        {
+            AlertSystem.Print("You do not yet have any notoriety, and therefore no enemies.");
+        }
+        else
+        {
+            int maxReduction = Mathf.FloorToInt(GameplayManager.dummyData.notoriety * 0.75f);
+            int reduction = Random.Range(1, maxReduction) + GameplayManager.dummyData.level;
+            string alertMessage = $"You reduced your notoriety by {reduction}.";
+
+            GameplayManager.dummyData.notoriety -= reduction;
+            int startingNetwork = GameplayManager.dummyData.network;
+            int currentNetwork = startingNetwork -= reduction;
+
+            while (currentNetwork < 0)
+            {
+                currentNetwork += 5;
+
+                if (GameplayManager.dummyData.cultMembers.Count > 1)
+                {
+                    Human cultist = GameplayManager.dummyData.cultMembers[Random.Range(0, GameplayManager.dummyData.cultMembers.Count)];
+                    alertMessage += $"\n{cultist.name} was lost to the enemy.";
+                    GameplayManager.dummyData.cultMembers.Remove(cultist);
+                }
+                else
+                {
+
+                }
+            }
+
+            if (currentNetwork > startingNetwork)
+            {
+                currentNetwork = startingNetwork - 1;
+            }
+
+            alertMessage += $"\nYour network decreased by " + (startingNetwork - currentNetwork);
+
+            AlertSystem.Force(alertMessage, () =>
+            {
+                HandleEndeavourPoints();
+            });
+        }
     }
 
     internal static void RecruitMembers(Location currentLocation)
