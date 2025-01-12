68fe36e9d7a21eb8077b4b24f262e941827dccb1
Renatuscape <rrjonsberg@gmail.com>
2025-01-12 17:28:34 +0100

    Add script for controlling item prefab

---
+++ Assets/Scripts/UI/Prefabs/ItemUiPrefab.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemUiPrefab: MonoBehaviour
+{
+    public Item item;
+    public Image image;
+    public Button button;
+    public TextMeshProUGUI itemName;
+    public TextMeshProUGUI itemStats;
+
+    public void SetDisplay(Item item)
+    {
+        this.item = item;
+        UpdateDisplayInfo();
+    }
+
+    void UpdateDisplayInfo()
+    {
+        itemName.text = item.name;
+        itemStats.text = $"LOR:{item.lore:D2} STR:{item.strength:D2} MGC:{item.magick:D2}";
+    }
+}
