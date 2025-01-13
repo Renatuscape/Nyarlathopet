48398ad628586a4c80263433e2d8c76b8e861d0a
Renatuscape <silarcta@gmail.com>
2025-01-13 15:43:08 +0100

    Add static commands for ritual functions

---
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public static class RitualController
+{
+    public static RitualManager manager;
+
+    public static void EnableRitualMenu()
+    {
+        manager.ritualCanvas.gameObject.SetActive(true);
+    }
+
+    public static void AbortRitual()
+    {
+        manager.ritualCanvas.gameObject.SetActive(false);
+        manager.ResetRitual();
+    }
+
+    public static void SacrificeArtefact(Item item)
+    {
+        GameplayManager.dummyData.inventory.Remove(item);
+    }
+
+    public static void SacrificeCultist()
+    {
+        manager.SacrificeCultist();
+    }
+
+    internal static bool CheckIfCultistSelected(out Human selectedCultist)
+    {
+        selectedCultist = manager.activeCultist?.human;
+
+        return manager.activeCultist != null;
+    }
+}
