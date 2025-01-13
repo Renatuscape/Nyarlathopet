b96b9c4e0c1a7b5ebe2462bce81b96910e125255
Renatuscape <silarcta@gmail.com>
2025-01-13 15:43:55 +0100

    Add class for sacrifice button functionality

---
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnSacrificeCultist.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BtnSacrificeCultist : MonoBehaviour
+{
+    public void BtnSacrifice()
+    {
+        if (RitualController.CheckIfCultistSelected(out var cultist))
+        {
+            AlertSystem.Prompt($"Sacrifice {cultist.name} to Nyarlathotep?\nThis action cannot be undone, and may drive witnesses to madness.", () =>
+            {
+                RitualController.SacrificeCultist();
+            });
+        }
+        else
+        {
+            AlertSystem.Print("Choose whom you wish to sacrifice first.");
+        }
+    }
+}
