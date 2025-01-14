0a51f5706c7dd25304ab2071b462b2ff6ace99b7
Renatuscape <silarcta@gmail.com>
2025-01-14 15:11:23 +0100

    Add prefab for pageination buttons and text

---
+++ Assets/Scripts/UI/Prefabs/PageinationPrefab.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageinationPrefab : MonoBehaviour
+{
+    public Button btnRight;
+    public Button btnLeft;
+
+    public TextMeshProUGUI pageText;
+}
