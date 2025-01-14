3520817b8858e885f8d7c62dfa136fea7d0ab2d6
Renatuscape <silarcta@gmail.com>
2025-01-14 15:10:41 +0100

    Add indices for cultist and item lists. Add method to enable ritual menu, moving display updates away from OnEnable. Add method to update cultists and check for insanity

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualManager.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualManager.cs
@@ -14,6 +14,8 @@ public class RitualManager : MonoBehaviour
     public TextMeshProUGUI ritualValues;
     [SerializeField]
     private Horror ritualState;
+    int cultistIndex;
+    int itemIndex;
 
     private void Start()
     {
@@ -21,10 +23,14 @@ public class RitualManager : MonoBehaviour
         ritualCanvas.gameObject.SetActive(false);
     }
 
-    private void OnEnable()
+    public void EnableRitualMenu()
     {
+        Report.Write(name, "Setting up ritual menu display.");
         UpdateDisplayedRitualValues();
+        UpdateCultists();
+        UpdateItems();
         ResetRitual();
+        ritualCanvas.gameObject.SetActive(true);
     }
 
     public void ResetRitual()
@@ -36,17 +42,42 @@ public class RitualManager : MonoBehaviour
 
     internal void SacrificeCultist()
     {
-        if (activeCultist == null ) { return; }
+        if (activeCultist == null) { return; }
 
         RitualSacrificeHelper.SacrificeCultist(ritualState, activeCultist.human, out var report);
 
         AlertSystem.Force(report, () =>
         {
-            // Check if any cultists have gone insane and remove them from cult
+            CheckForInsaneCultists();
+            UpdateCultists();
         });
 
         UpdateDisplayedRitualValues();
-        // Add cultist prefab refresh
+    }
+
+    void CheckForInsaneCultists()
+    {
+        List<Human> insaneCultists = new();
+        GameplayManager.dummyData.cultMembers.ForEach(c =>
+        {
+            if (c.sanity <= 0) { insaneCultists.Add(c); }
+        });
+
+        if (insaneCultists.Count == 1)
+        {
+            AlertSystem.Print(insaneCultists[0] + " has gone insane.");
+        }
+        else if (insaneCultists.Count > 1)
+        {
+            string alert = "Several cultists have gone insane. You lost ";
+            insaneCultists.ForEach(m => alert += m.name + ", ");
+
+            alert = alert.Trim(' ').Trim(',');
+
+            AlertSystem.Print(alert);
+        }
+
+        insaneCultists.ForEach(c => GameplayManager.dummyData.cultMembers.Remove(c));
     }
 
     void UpdateDisplayedRitualValues()
@@ -57,9 +88,15 @@ public class RitualManager : MonoBehaviour
             $"{ritualState.magick:D2}\n" +
             $"{ritualState.rage:D2}";
     }
-}
 
-public static class ListHelper
-{
+    void UpdateCultists()
+    {
+        Report.Write(name, "Updating displayed cultists.");
+        ListHelper.DisplayHumansFromIndex(cultistIndex, GameplayManager.dummyData.cultMembers, cultistSlots);
+    }
 
+    void UpdateItems()
+    {
+        ListHelper.DisplayItemsFromIndex(cultistIndex, GameplayManager.dummyData?.inventory ?? new(), itemSlots);
+    }
 }
