e83ad0d5cf70c61f811b8e29dfba33067e6c0223
Renatuscape <silarcta@gmail.com>
2025-01-15 14:19:48 +0100

    Move abort prompt from button to here. Check if any sacrifices have been made, and skip prompt if not

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualController.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualController.cs
@@ -11,8 +11,21 @@ public static class RitualController
 
     public static void AbortRitual()
     {
-        manager.ritualCanvas.gameObject.SetActive(false);
-        manager.ResetRitual();
+        if (manager.sacrificesMade > 0)
+        {
+            Report.Write("RitualController", manager.sacrificesMade + " sacrifices were already made. Prompt before exiting.");
+            AlertSystem.Prompt("ABANDON PROGRESS AND ABORT RITUAL?\nAll amassed power will be lost. Any sacrifices made will be for naught.", () =>
+            {
+                manager.ritualCanvas.gameObject.SetActive(false);
+                manager.ResetRitual();
+            });
+        }
+        else
+        {
+            Report.Write("RitualController", manager.sacrificesMade + " sacrifices made. Exiting without prompt.");
+            manager.ritualCanvas.gameObject.SetActive(false);
+            manager.ResetRitual();
+        }
     }
 
     public static void SacrificeArtefact(Item item)
