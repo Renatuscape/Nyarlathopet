3c9233b4dfa3acb7fcbc8c25ec327e3531b3b113
Renatuscape <silarcta@gmail.com>
2025-01-15 14:18:19 +0100

    Handle alert prompt in RitualController

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnAbortRitual.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnAbortRitual.cs
@@ -4,9 +4,6 @@ public class BtnAbortRitual : MonoBehaviour
 {
     public void BtnAbort()
     {
-        AlertSystem.Prompt("ABANDON PROGRESS AND ABORT RITUAL?\nAll amassed power will be lost. Any sacrifices made will be for naught.", () =>
-        {
-            RitualController.AbortRitual();
-        });
+        RitualController.AbortRitual();
     }
 }
