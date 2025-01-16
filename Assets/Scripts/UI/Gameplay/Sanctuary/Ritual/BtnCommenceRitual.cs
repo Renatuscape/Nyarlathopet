6bbccf87b6c6828cef135528fc512e1b2b7dc34e
Renatuscape <silarcta@gmail.com>
2025-01-16 15:48:53 +0100

    Add button functionality for commencing ritual

---
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnCommenceRitual.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BtnCommenceRitual : MonoBehaviour
+{
+    public void BtnCommence()
+    {
+        AlertSystem.Prompt("Commence the summoning with these offerings and sacrifices?", () =>
+        {
+            RitualController.CommenceRitual();
+        });
+    }
+}
