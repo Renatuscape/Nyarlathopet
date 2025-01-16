4439e2f57f6c3081e2cd053dc8eadb9b89c9f041
Renatuscape <silarcta@gmail.com>
2025-01-16 15:51:10 +0100

    Fix logical bug where null items would be checked for type and throw error

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnOfferArtefact.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnOfferArtefact.cs
@@ -6,18 +6,21 @@ public class BtnOfferArtefact : MonoBehaviour
 {
     public void BtnOffer()
     {
-        if (RitualController.CheckIfItemSelected(out var artefact) && artefact.type == ItemType.Artefact)
+        if (RitualController.CheckIfItemSelected(out var artefact))
         {
-            AlertSystem.Prompt($"Offer {artefact.name} to Nyarlathotep?\nThe artefact will be lost for good.", () =>
+            if (artefact.type != ItemType.Artefact)
             {
-                RitualController.OfferArtefact();
-            });
+                AlertSystem.Print("Only artefacts may be offered during the ritual.");
+            }
+            else
+            {
+                AlertSystem.Prompt($"Offer {artefact.name} to Nyarlathotep?\nThe artefact will be lost for good.", () =>
+                {
+                    RitualController.OfferArtefact();
+                });
+            }
         }
-        else if (artefact.type != ItemType.Artefact)
-        {
-            AlertSystem.Print("Only artefacts may be offered during the ritual.");
-        }
-        else if (GameplayManager.dummyData.cultMembers.Count > 0)
+        else if (GameplayManager.dummyData.inventory.Count > 0)
         {
             AlertSystem.Print("Choose an artefact to offer.");
         }
