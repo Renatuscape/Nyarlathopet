eb84db9b9c8733b7e44d52e11afb85bfbbb57253
Renatuscape <silarcta@gmail.com>
2025-01-16 15:51:47 +0100

    Add CommenceRitual method and RemoveInsaneCultists method. Add documentation and tidy up access modifiers

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualController.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/RitualController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Class works as a layer between buttons and RitualManager, allowing the manager to be easily called from elsewhere in the scene
+using System.Collections.Generic;
 
 public static class RitualController
 {
@@ -38,17 +39,50 @@ public static class RitualController
         manager.SacrificeCultist();
     }
 
-    internal static bool CheckIfCultistSelected(out Human selectedCultist)
+    public static bool CheckIfCultistSelected(out Human selectedCultist)
     {
         selectedCultist = manager.activeCultist?.human;
 
         return manager.activeCultist != null;
     }
 
-    internal static bool CheckIfItemSelected(out Item selectedArtefact)
+    public static bool CheckIfItemSelected(out Item selectedArtefact)
     {
         selectedArtefact = manager.activeItem?.item;
 
         return manager.activeItem != null;
+    }
+
+    public static void CommenceRitual()
+    {
+        manager.CommenceRitual();
+    }
+
+    public static void RemoveInstaneCultists(out string report)
+    {
+        List<Human> insaneCultists = new();
+
+        GameplayManager.dummyData.cultMembers.ForEach(c =>
+        {
+            if (c.sanity <= 0) { insaneCultists.Add(c); }
+        });
+
+        if (insaneCultists.Count == 1)
+        {
+            report = insaneCultists[0].name + " has gone insane.";
+        }
+        else if (insaneCultists.Count > 1)
+        {
+            string alert = "Several cultists have gone insane. You lost ";
+            insaneCultists.ForEach(m => alert += m.name + ", ");
+
+            report = alert.Trim(' ').Trim(',');
+        }
+        else
+        {
+            report = "";
+        }
+
+        insaneCultists.ForEach(c => GameplayManager.dummyData.cultMembers.Remove(c));
     }
 }
