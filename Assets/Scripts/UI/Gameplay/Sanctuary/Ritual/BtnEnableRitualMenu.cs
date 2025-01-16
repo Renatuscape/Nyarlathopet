8ca0080764fe576ca993d6c606d5c1e2c49f6437
Renatuscape <silarcta@gmail.com>
2025-01-16 15:50:44 +0100

    Check if player has enough EP before starting a ritual, and display alert

--- Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnEnableRitualMenu.cs
+++ Assets/Scripts/UI/Gameplay/Sanctuary/Ritual/BtnEnableRitualMenu.cs
@@ -1,10 +1,17 @@
 ﻿using UnityEngine;
 
-public class BtnEnableRitualMenu: MonoBehaviour
+public class BtnEnableRitualMenu : MonoBehaviour
 {
     public void BtnStartRitual()
     {
-        Report.Write(name, "Opening ritual menu.");
-        RitualController.EnableRitualMenu();
+        if (GameplayManager.EndeavourPoints == GameplayManager.MaxEndeavourPoints)
+        {
+            Report.Write(name, "Opening ritual menu.");
+            RitualController.EnableRitualMenu();
+        }
+        else
+        {
+            AlertSystem.Print($"The summoning ritual must commence at the beginning of a new month, when the moon and stars are correct.\nReturn when you have {GameplayManager.MaxEndeavourPoints} EP.");
+        }
     }
 }
