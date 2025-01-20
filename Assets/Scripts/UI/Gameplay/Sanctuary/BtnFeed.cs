72e4364eda77aac73aec9975e5a91702a47a6ab6
Renatuscape <silarcta@gmail.com>
2025-01-20 16:02:23 +0100

    Add class for feed button functionality

---
+++ Assets/Scripts/UI/Gameplay/Sanctuary/BtnFeed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BtnFeed : MonoBehaviour
+{
+    public void BtnFeedPet()
+    {
+        PetController.FeedPet();
+    }
+}
