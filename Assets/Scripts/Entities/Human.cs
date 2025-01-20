86f3477968db901264feeddf9da16534e80cf3f4
Renatuscape <silarcta@gmail.com>
2025-01-20 15:55:48 +0100

    Rename magick to occultism. Sort skills. Add print method

--- Assets/Scripts/Entities/Human.cs
+++ Assets/Scripts/Entities/Human.cs
@@ -1,10 +1,22 @@
 [System.Serializable]
 public class Human : Creature
 {
-    public int magick;
-    public int strength;
+    public int occultism;
     public int lore;
+    public int strength;
     public int sanity;
     public int funds;
     public string origin;
+
+    public string GetStatPrintOut(bool abbreviate, bool isList)
+    {
+        return
+            $"{(abbreviate ? "OCL:" : "Occultism ")}{occultism:D2}{GetBreak()}" +
+            $"{(abbreviate ? "LOR:" : "Lore ")}{lore:D2}{GetBreak()}" +
+            $"{(abbreviate ? "STR:" : "Strength ")}{strength:D2}{GetBreak()}" +
+            $"{(abbreviate ? "FND:" : "Funds ")}{funds:D2}{GetBreak()}" +
+            $"{(abbreviate ? "SAN:" : "Sanity ")}{sanity:D2}";
+
+        string GetBreak() { return isList ? "\n" : (abbreviate ? " " : ", "); }
+    }
 }
