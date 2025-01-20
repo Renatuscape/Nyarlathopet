4dc9807819e4f438978fff71342f21bb636f587d
Renatuscape <silarcta@gmail.com>
2025-01-20 15:57:16 +0100

    Add helper class to adjust human stats

---
+++ Assets/Scripts/Tools/CreatureStats.cs
@@ -0,0 +1,11 @@
+﻿public class CreatureStats
+{
+    public int mythos;
+    public int intrigue;
+
+    public int magick;
+    public int abstraction;
+
+    public int strength;
+    public int rage;
+}
