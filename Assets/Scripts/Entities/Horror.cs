64d664b7c37eceea4ca61e03cde5ef62d470091c
Renatuscape <silarcta@gmail.com>
2025-01-20 15:55:31 +0100

    Add mythos skill. Sort skill list. Add print method for stats and method to apply stat changes

--- Assets/Scripts/Entities/Horror.cs
+++ Assets/Scripts/Entities/Horror.cs
@@ -5,11 +5,12 @@
 public class Horror : Creature
 {
     public string id = "-1";
-    public int rage; // Pets use this as starting value. Masks add this to the total upon being summoned
+    public int mythos;
     public int intrigue;
-    public int abstraction;
     public int magick;
+    public int abstraction;
     public int strength;
+    public int rage; // Pets use this as starting value. Masks add this to the total upon being summoned
     public int sanityLoss; // Max potential sanity loss upon witnessing the horror
 
     public Horror Clone()
@@ -25,5 +26,29 @@ public class Horror : Creature
             intrigue = intrigue,
             strength = strength
         };
+    }
+
+    public string GetStatPrintOut(bool abbreviate, bool isList)
+    {
+        return $"{(abbreviate ? "MTH:" : "Myhtos ")}{mythos:D2}{GetBreak()}" +
+            $"{(abbreviate ? "ITR:" : "Intrigue ")}{intrigue:D2}{GetBreak()}" +
+            $"{(abbreviate ? "MGC:" : "Magick ")}{magick:D2}{GetBreak()}" +
+            $"{(abbreviate ? "ABS:" : "Abstraction ")}{abstraction:D2} {GetBreak()}" +
+            $"{(abbreviate ? "STR:" : "Strength ")}{strength:D2} {GetBreak()}" +
+            $"{(abbreviate ? "RGE:" : "Rage ")}{rage:D2}";
+
+        string GetBreak() { return isList ? "\n" : (abbreviate ? " " : ", "); }
+    }
+
+    public void ApplyStatChanges(CreatureStats changes)
+    {
+        mythos += changes.mythos;
+        intrigue += changes.intrigue;
+
+        magick += changes.magick;
+        abstraction += changes.abstraction;
+
+        strength += changes.strength;
+        rage += changes.rage;
     }
 }
