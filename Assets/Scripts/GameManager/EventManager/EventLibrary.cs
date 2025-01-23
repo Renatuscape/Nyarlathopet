f1178ec6b70b66a1cdb177b108672946c6d9adbe
Renatuscape <silarcta@gmail.com>
2025-01-23 15:59:58 +0100

    Improve setup for event logic

--- Assets/Scripts/GameManager/EventManager/EventLibrary.cs
+++ Assets/Scripts/GameManager/EventManager/EventLibrary.cs
@@ -4,92 +4,207 @@ using System.Linq;
 
 public class EventData
 {
-    public string name;
+    public Event type;
     public int priority;
-    public int minimumCultists;
+    public Func<bool> condition;
     public Action action;
 
     public void Execute()
     {
-        Report.Write(name, "Executing action.");
+        Report.Write(type.ToString(), "Executing action.");
         action.Invoke();
+    }
+
+    public bool CheckCondition()
+    {
+        return condition == null || condition.Invoke();
     }
 }
 
 public static class EventLibrary
 {
-    public static readonly List<EventData> events = new()
+    static readonly Dictionary<Event, EventData> events = new()
     {
-        new()
         {
-            name = "CultistMutiny",
-            priority = 0, // Highest priority
-            minimumCultists = 1,
-            action = () =>
+            Event.NoLeader,
+            new()
             {
-                HandleEndEvent("EnemyRaid");
+                type = Event.NoLeader,
+                priority = 0,
+                condition = () =>
+                {
+                    return GameplayManager.dummyData.cultLeader == null && GameplayManager.dummyData.cultMembers.Count > 0;
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Placeholder for Choose New Leader event.", () =>
+                    {
+                        // Placeholder before a selection menu can be made
+                        var newLeader = GameplayManager.dummyData.cultMembers.First();
+                        GameplayManager.dummyData.cultLeader = newLeader;
+                        GameplayManager.dummyData.cultMembers.Remove(newLeader);
+                        AlertSystem.Print(newLeader.name + " became the new cult leader.");
+
+                        HandleEndEvent("NoLeader");
+                    });
+                }
             }
         },
-        new()
         {
-            name = "ChooseNewLeader",
-            priority = 1, // Second highest priority
-            minimumCultists = 0, // Game over if this initiates with 0 cultists
-            action = () =>
+            Event.CultMutiny,
+            new()
             {
-                AlertSystem.Force("Placeholder for Choose New Leader event.", () =>
+                type = Event.CultMutiny,
+                priority = 1,
+                condition = () =>
                 {
-                    if (GameplayManager.dummyData.cultMembers.Count < 1)
+                    return GameplayManager.mutineer != null
+                    && GameplayManager.dummyData.cultLeader != null
+                    && GameplayManager.dummyData.cultMembers.Count > 0;
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Cultist mutiny placeholder", () =>
                     {
-                        Report.Write("EventLibrary", "No cult members to replace old leader. Game is over.");
-                    }
-                    else
+                        HandleEndEvent("CultMutiny");
+                    });
+                }
+            }
+        },
+        {
+            Event.PetNotFed,
+            new()
+            {
+                type = Event.PetNotFed,
+                priority = 10,
+                condition = () =>
+                {
+                    return GameplayManager.CheckActivePet() && !GameplayManager.isPetFed;
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Pet not fed placeholder", () =>
                     {
-                        GameplayManager.dummyData.cultLeader = GameplayManager.dummyData.cultMembers.First();
+                        HandleEndEvent("PetNotFed");
+                    });
+                }
+            }
+        },
+        {
+            Event.PetOnRampage,
+            new()
+            {
+                type = Event.PetOnRampage,
+                priority = 80,
+                condition = () =>
+                {
+                    if (GameplayManager.CheckActivePet())
+                    {
+                        return false;
                     }
 
-                    HandleEndEvent("ChooseNewLeader");
-                });
+                    int rage = GameplayManager.dummyData.currentPet.rage;
+                    int rageThreshold = 150 - rage - GameplayManager.dummyData.level; // Danger of rampage increases with player level
+
+                    return UnityEngine.Random.Range(0, 100) > rageThreshold;
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Pet on rampage placeholder", () =>
+                    {
+                       HandleEndEvent("PetOnRampage");
+                    });
+                }
             }
         },
-        new()
         {
-            name = "PetOnRampage",
-            priority = 80,
-            minimumCultists = 0,
-            action = () =>
+            Event.CultRaided,
+            new()
             {
-                HandleEndEvent("PetOnRampage");
+                type = Event.CultRaided,
+                priority = 100, // Lowest priority
+                condition = () =>
+                {
+                    int raidThreshold = 125 - GameplayManager.dummyData.notoriety;
+                    return UnityEngine.Random.Range(0, 100) > raidThreshold;
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Cult raided placeholder", () =>
+                    {
+                        HandleEndEvent("CultRaided");
+                    });
+                }
             }
         },
-        new()
         {
-            name = "EnemyRaid",
-            priority = 100, // Lowest priority
-            minimumCultists = 0,
-            action = () =>
+            Event.PetNotSummoned,
+            new()
             {
-                HandleEndEvent("EnemyRaid");
+                type = Event.PetNotSummoned,
+                priority = 50,
+                condition = () =>
+                {
+                    return GameplayManager.dummyData.currentPet == null
+                    && GameplayManager.dummyData.cultMembers.Count > 0; // No point if there are no members to be mad
+                },
+                action = () =>
+                {
+                    AlertSystem.Force("Pet not summoned placeholder", () =>
+                    {
+                        HandleEndEvent("PetNotSummoned");
+                    });
+                }
             }
         },
-        new()
         {
-            name = "PetNotFed",
-            priority = 10,
-            minimumCultists = 0,
-            action = () =>
+            Event.NewMonth,
+            new()
             {
-                HandleEndEvent("PetNotFed");
+                type = Event.NewMonth,
+                priority = int.MaxValue, // Always play this last
+                condition = () =>
+                {
+                    return !(GameplayManager.dummyData.cultLeader == null && GameplayManager.dummyData.cultMembers.Count < 1);
+                },
+                action = () =>
+                {
+                    AlertSystem.Force(Repository.GetText("ROUND-CLEAR0"),() => {
+                        HandleEndEvent(Event.NewMonth.ToString());
+                    });
+                }
             }
         },
-        new()
         {
-            name = "NoCurrentPet",
-            priority = 50,
-            minimumCultists = 0,
-            action = () =>
+            Event.GameOverDead,
+            new()
             {
-                HandleEndEvent("NoCurrentPet");
+                type = Event.GameOverDead,
+                priority = int.MinValue,
+                condition = () =>
+                {
+                    return GameplayManager.dummyData.cultLeader == null && GameplayManager.dummyData.cultMembers.Count < 1;
+                },
+                action = () =>
+                {
+                    EventHelper.PromptGameOverOptions(Repository.GetText("END-DEAD"));
+                }
+            }
+        },
+        {
+            Event.GameOverInsane,
+            new()
+            {
+                type = Event.GameOverInsane,
+                priority = int.MinValue,
+                condition = () =>
+                {
+                    return GameplayManager.dummyData.cultLeader == null && GameplayManager.dummyData.cultMembers.Count < 1;
+                },
+                action = () =>
+                {
+                    EventHelper.PromptGameOverOptions(Repository.GetText("END-INSANE"));
+                }
             }
         }
     };
@@ -99,4 +214,24 @@ public static class EventLibrary
         Report.Write("EventLibrary", "Completed " + eventName);
         EventController.ExecuteNextInQueue();
     }
+
+    public static List<EventData> GetEventQueue(PlayerData data)
+    {
+        List<EventData> qualifyingEvents = events.Where(e => e.Value.CheckCondition()).Select(e => e.Value).ToList();
+
+        return qualifyingEvents;
+    }
+}
+
+public enum Event
+{
+    NewMonth,
+    PetNotFed,
+    PetOnRampage,
+    PetNotSummoned,
+    NoLeader,
+    CultRaided,
+    CultMutiny,
+    GameOverInsane,
+    GameOverDead
 }
