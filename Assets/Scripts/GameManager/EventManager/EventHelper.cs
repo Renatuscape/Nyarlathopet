f8de53328b177d2b09f46f7dae855ee9f66dd32d
Renatuscape <silarcta@gmail.com>
2025-01-23 15:55:12 +0100

    Add event helper class for reusable event methods

---
+++ Assets/Scripts/GameManager/EventManager/EventHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class EventHelper
+{
+    public static void PromptGameOverOptions(string textAlert)
+    {
+        Player.SetPlayerData(null);
+        AlertSystem.Choice(textAlert, new(){
+            ("", () => {SceneManager.LoadScene("NewGame");})
+        });
+    }
+}
