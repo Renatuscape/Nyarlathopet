229e13f5b248186cf34b4c862e1b589ef6529bc3
Renatuscape <silarcta@gmail.com>
2025-01-29 15:53:46 +0100

    Add class with helper methods for updating dummy data

---
+++ Assets/Scripts/GameManager/Gameplay/DummyDataUpdater.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public static class DummyDataUpdater
+{
+    public static int UpdateNotoriety(int points)
+    {
+        return UpdateStat(ref GameplayManager.dummyData.notoriety, points);
+    }
+
+    public static int UpdateNetwork(int points)
+    {
+        return UpdateStat(ref GameplayManager.dummyData.network, points);
+    }
+
+    public static int UpdateRage(int points, bool decrease)
+    {
+        if (!GameplayManager.CheckIsPetActive()) { return 0; }
+
+        if (decrease && points > 0)
+        {
+            points = -points;
+        }
+
+        return UpdateStat(ref GameplayManager.dummyData.currentPet.rage, points);
+    }
+
+    public static int UpdateSanity(int points)
+    {
+        if (GameplayManager.dummyData.cultLeader == null) { return 0; }
+        return UpdateStat(ref GameplayManager.dummyData.cultLeader.sanity, points);
+    }
+
+    static int UpdateStat(ref int stat, int points)
+    {
+        int original = stat;
+        stat = Math.Clamp(original + points, 0, 99);
+        return stat - original;
+    }
+}
