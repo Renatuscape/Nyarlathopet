1a99e8e64f2f5f9db030ed4f1989191889148ad6
Renatuscape <silarcta@gmail.com>
2025-01-30 12:49:26 +0100

    Add text class to concisely get dynamic text snippets

---
+++ Assets/Scripts/Tools/Text.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+public static class Text
+{
+    public static string Get(string tag)
+    {
+        var entry = Repository.displayText?.entries?.FirstOrDefault(e => e[0] == tag);
+
+        if (entry == null)
+        {
+            Report.Write("Tags", tag + " returned null.");
+        }
+
+        return entry != null ? entry[1] : "▓░▓";
+    }
+}
