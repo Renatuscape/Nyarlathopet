a635bb74097bc957d4ee4f30c6190e8900535c28
Renatuscape <silarcta@gmail.com>
2025-01-30 12:42:58 +0100

    Replace unused print method with new using tags

--- Assets/Scripts/Entities/Human.cs
+++ Assets/Scripts/Entities/Human.cs
@@ -8,20 +8,11 @@ public class Human : Creature
     public int funds;
     public string origin;
 
-    public string GetStatPrintOut(bool abbreviate, bool isList)
-    {
-        return
-            $"{(abbreviate ? "OCL:" : "Occultism ")}{occultism:D2}{GetBreak()}" +
-            $"{(abbreviate ? "LOR:" : "Lore ")}{lore:D2}{GetBreak()}" +
-            $"{(abbreviate ? "STR:" : "Strength ")}{strength:D2}{GetBreak()}" +
-            $"{(abbreviate ? "FND:" : "Funds ")}{funds:D2}{GetBreak()}" +
-            $"{(abbreviate ? "SAN:" : "Sanity ")}{sanity:D2}";
-
-        string GetBreak() { return isList ? "\n" : (abbreviate ? " " : ", "); }
-    }
-
     public string Print(bool lineBreak = true, bool includeSanity = false, bool includeFunds = false, bool noName = false)
     {
-        return $"{(noName? "": name)}{(lineBreak ? "\n" : (noName ? "" : " "))}{(includeSanity ? $"SAN:{sanity:D2} " : "")}{(includeFunds ? $"FND:{funds:D2} " : "")}OCC:{occultism:D2} LOR:{lore:D2} STR:{strength:D2}";
+        return $"{(noName? "": name)}{(lineBreak ? "\n" : (noName ? "" : " "))}" +
+            $"{(includeSanity ? $"{Tags.Get("SAN")}:{sanity:D2} " : "")}" +
+            $"{(includeFunds ? $"{Tags.Get("FNS")}:{funds:D2} " : "")}" +
+            $"{Tags.Get("OCL")}:{occultism:D2} {Tags.Get("LOR")}:{lore:D2} {Tags.Get("STR")}:{strength:D2}";
     }
 }
