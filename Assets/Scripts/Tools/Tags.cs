c7f0152be2495269e96edbf5de3bde7404ec99ab
Renatuscape <silarcta@gmail.com>
2025-01-30 12:49:08 +0100

    Add tags class to concisely retrieve dynamic tag text snippets

---
+++ Assets/Scripts/Tools/Tags.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+public static class Tags
+{
+    public static string Get(string tag)
+    {
+        var entry = Repository.displayText?.tags?.FirstOrDefault(e => e[0] == tag);
+
+        if (entry == null)
+        {
+            Report.Write("Tags", tag + " returned null.");
+        }
+
+        return entry != null ? entry[1] : "▓║▓┤▓░▓";
+    }
+}
