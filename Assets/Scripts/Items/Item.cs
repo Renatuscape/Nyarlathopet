b52d57b7dc65275b97693400b5d3dd15faf53741
Renatuscape <silarcta@gmail.com>
2025-01-30 12:48:19 +0100

    Add print class with tags

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -8,6 +8,13 @@ public class Item
     public int occultism;
     public int strength;
     public int lore;
+
+    public string Print(bool lineBreak = true, bool includeType = false, bool noName = false)
+    {
+        return $"{(noName ? "" : name)}{(lineBreak ? "\n" : (noName ? "" : " "))}" +
+            $"{(includeType ? $"{Tags.Get(type.ToString())} " : "")}" +
+            $"{Tags.Get("OCL")}:{occultism:D2} {Tags.Get("LOR")}:{lore:D2} {Tags.Get("STR")}:{strength:D2}";
+    }
 }
 
 public enum ItemType
