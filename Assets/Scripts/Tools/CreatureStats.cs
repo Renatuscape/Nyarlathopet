1fbe0fbd971d08c758e7711d80099ec9ecd5338c
Renatuscape <silarcta@gmail.com>
2025-02-03 13:18:55 +0100

    Add method to distribute stats randomly, primairly for building dummy data

--- Assets/Scripts/Tools/CreatureStats.cs
+++ Assets/Scripts/Tools/CreatureStats.cs
@@ -1,4 +1,8 @@
-﻿public class CreatureStats
+﻿using System.Linq;
+using UnityEngine;
+
+[System.Serializable]
+public class CreatureStats
 {
     public int mythos;
     public int intrigue;
@@ -8,4 +12,18 @@
 
     public int strength;
     public int rage;
+
+    public void AddRandomStats(int totalPoints)
+    {
+        var stats = typeof(CreatureStats)
+            .GetFields()
+            .Where(f => f.FieldType == typeof(int))
+            .ToArray();
+
+        for (int i = 0; i < totalPoints; i++)
+        {
+            stats[Random.Range(0, stats.Length)]
+                .SetValue(this, (int)stats[Random.Range(0, stats.Length)].GetValue(this) + 1);
+        }
+    }
 }
