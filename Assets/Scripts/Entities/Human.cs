4ae384fe25bf62210023b148283506a5b9e3a919
Renatuscape <silarcta@gmail.com>
2025-02-03 13:13:11 +0100

    Add methods to get level and apply stat changes

--- Assets/Scripts/Entities/Human.cs
+++ Assets/Scripts/Entities/Human.cs
@@ -17,4 +17,18 @@ public class Human : Creature
             $"{(includeFunds ? $"{Tags.Get("FNS")}:{funds:D2} " : "")}" +
             $"{Tags.Get("OCL")}:{occultism:D2} {Tags.Get("LOR")}:{lore:D2} {Tags.Get("STR")}:{strength:D2}";
     }
+
+    public int GetLevel()
+    {
+        return Math.Max(1, (occultism + lore + strength) / 2);
+    }
+
+    public void ApplyStatChanges(MundaneStats changes)
+    {
+        occultism = Math.Clamp(occultism + changes.occultism, 0, 99);
+        lore = Math.Clamp(lore + changes.lore, 0, 99);
+        strength = Math.Clamp(strength + changes.strength, 0, 99);
+        funds = Math.Clamp(funds + changes.funds, 0, 99);
+        sanity = Math.Clamp(sanity + changes.sanity, 0, 99);
+    }
 }
