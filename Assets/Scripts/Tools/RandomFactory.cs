77e792c7cde7e91f17c23f3f9fce04a93c1221dc
Renatuscape <silarcta@gmail.com>
2025-02-03 13:19:46 +0100

    Add class for various helper methods for random generation

---
+++ Assets/Scripts/Tools/RandomFactory.cs
@@ -0,0 +1,17 @@
+﻿public static class RandomFactory
+{
+    public static string GetRandomString(int length)
+    {
+        // Characters that are generally safe with Unity's fallback font
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?";
+        var random = new System.Random();
+
+        var result = new System.Text.StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            result.Append(validChars[random.Next(validChars.Length)]);
+        }
+
+        return result.ToString();
+    }
+}
