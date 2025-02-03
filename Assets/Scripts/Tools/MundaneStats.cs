8867533619cfd4cac396a1682677c828cf9a306c
Renatuscape <silarcta@gmail.com>
2025-02-03 13:19:21 +0100

    Add variables for sanity and funds, and make serialisable

--- Assets/Scripts/Tools/MundaneStats.cs
+++ Assets/Scripts/Tools/MundaneStats.cs
@@ -1,6 +1,9 @@
-﻿public class MundaneStats
+﻿[System.Serializable]
+public class MundaneStats
 {
+    public int sanity;
     public int lore;
     public int occultism;
     public int strength;
+    public int funds;
 }
