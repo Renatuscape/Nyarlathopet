eff2a5c10aa3d76263ae6ea4ea77c7bc073beda7
Renatuscape <silarcta@gmail.com>
2025-02-06 16:10:29 +0100

    Add more options for menu buttons

--- Assets/Scripts/UI/Hotbar/BtnPromptMainMenu.cs
+++ Assets/Scripts/UI/Hotbar/BtnPromptMainMenu.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,10 +9,54 @@ public class BtnPromptMainMenu : MonoBehaviour
 {
     public void BtnPromptMain()
     {
-        AlertSystem.Prompt("Return to main menu?\nProgress on the current month will be lost.",
+        AlertSystem.Prompt(Text.Get("MENU-MAINP"),
             () =>
             {
                 SceneManager.LoadScene("MainMenu");
             });
+    }
+
+    public void BtnPromptMenuOptions()
+    {
+        AlertSystem.Choice(Text.Get("MENU-OPTIT"), new()
+        {
+            (Text.Get("OPTION-LANG"), () => PromptChooseLanguage()),
+            (Text.Get("OPTION-MMENU"), () => BtnPromptMain()),
+        }, false);
+    }
+
+    public void BtnPromptSettings()
+    {
+        AlertSystem.Choice(Text.Get("MENU-OPTIT"), new()
+        {
+            (Text.Get("OPTION-LANG"), () => PromptChooseLanguage()),
+        }, false);
+    }
+
+    void PromptChooseLanguage()
+    {
+        List<(string, Action)> options = new();
+        var languages = Enum.GetValues(typeof(Language)).Cast<Language>().ToArray();
+
+        foreach (var language in languages)
+        {
+            string label = language != GlobalSettings.language ? language.ToString() : $"[{language.ToString()}]";
+            options.Add((language.ToString(), () => ChooseLanguage(language)));
+        }
+
+        AlertSystem.Choice(Text.Get("SETT-LANGP"), options, false);
+    }
+
+    void ChooseLanguage(Language language)
+    {
+        if (language != GlobalSettings.language)
+        {
+            GlobalSettings.language = language;
+            AlertSystem.Print($"{Text.Get("SETT-LANGCA")} {language.ToString()}. {Text.Get("SETT-LANGCB")}");
+        }
+        else
+        {
+            AlertSystem.Print($"{language.ToString()} {Text.Get("SETT-LANGERR")}");
+        }
     }
 }
