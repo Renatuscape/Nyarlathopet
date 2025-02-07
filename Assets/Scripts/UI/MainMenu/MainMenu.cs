7708c626eed6c4ab3b018bb76ecda768e7489e82
Renatuscape <silarcta@gmail.com>
2025-02-07 13:44:04 +0100

    Add references to hotbar button textmeshes and set the language on awake

--- Assets/Scripts/UI/MainMenu/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu/MainMenu.cs
@@ -8,10 +8,17 @@ public class MainMenu : MonoBehaviour
     public PlayerData playerData;
     public TextMeshProUGUI titleCultName;
     public TextMeshProUGUI titleLeaderName;
+    public TextMeshProUGUI txtBtnContinue;
+    public TextMeshProUGUI txtBtnNewGame;
+    public TextMeshProUGUI txtBtnSettings;
 
-    void Start()
+    void Awake()
     {
         UpdateDisplayData();
+
+        txtBtnContinue.text = Text.Get("BtnMM-CONT");
+        txtBtnNewGame.text = Text.Get("BtnMM-NEWG");
+        txtBtnSettings.text = Text.Get("BtnMM-SETT");
     }
 
     void UpdateDisplayData()
